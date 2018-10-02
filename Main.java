c5b22252e42c012b644072f88e7e6914a062d2dd
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2018-10-02 09:40:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,14 +15,20 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Zmiana wielkości liter");
         Button btn1 = new Button();
+        btn1.setMinSize(200,50);
         btn1.setText("Przetwarzaj");
         Button btn2 = new Button();
+        btn2.setMinSize(200,50);
         btn2.setText("Wyczyść źródło");
         Button btn3 = new Button();
+        btn3.setMinSize(200,50);
+        btn3.setDisable(true);
         btn3.setText("Pomoc");
         Button btn4 = new Button();
+        btn4.setMinSize(200,50);
         btn4.setText("O programie");
         Button btn5 = new Button();
+        btn5.setMinSize(200,50);
         btn5.setText("Zapisz i zakończ");
 
         StackPane root = new StackPane();
@@ -35,28 +41,29 @@ public class Main extends Application {
         Text Tools = new Text("Narzędzia");
         Text Source = new Text("Źródło");
         TextField SourceTextField = new TextField();
+        SourceTextField.setMinSize(200,100);
         Text Output = new Text("Wynik");
         TextField OutputTextField = new TextField();
+        OutputTextField.setMinSize(200,100);
 
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(5);
         grid.setVgap(5);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setPadding(new Insets(5, 5, 5, 5));
 
-        Scene scene = new Scene(grid, 500, 275);
+        Scene scene = new Scene(grid, 500, 325);
 
-        grid.add(Tools, 0, 0);
-        grid.add(btn1, 0, 1);
-        grid.add(btn2, 0, 2);
-        grid.add(btn3, 0, 3);
-        grid.add(btn4, 0, 4);
-        grid.add(btn5, 0, 5);
-        grid.add(Source, 1, 1);
-        grid.add(SourceTextField, 1, 2);
-        grid.add(Output, 1, 3);
-        grid.add(OutputTextField, 1, 4);
-
+        grid.add(Tools, 0, 2);
+        grid.add(btn1, 0, 3);
+        grid.add(btn2, 0, 4);
+        grid.add(btn3, 0, 5);
+        grid.add(btn4, 0, 6);
+        grid.add(btn5, 0, 7);
+        grid.add(Source, 1, 0);
+        grid.add(SourceTextField, 1, 1, 1, 4);
+        grid.add(Output, 1, 5);
+        grid.add(OutputTextField, 1, 6, 1, 4);
 
         primaryStage.setScene(scene);
         primaryStage.show();
